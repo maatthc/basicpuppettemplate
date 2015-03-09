287db3618ceb8483f3050735a389562bbcc6820c
root <root@master.localdomain>
2015-03-09 18:25:11 +1100

    Script to rename module implemented

--- changeModuloname.sh
+++ changeModuloname.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-find ./ -type f | xargs sed -i  's/basicpuppettemplate/basicpuppettemplate/g'
+use () 
+{
+        echo "Use: $0 newname_for_the_module [current_name]"
+}
+
+if [ -z $1 ]
+then
+        echo "Missing new name. Exiting.."
+        use
+        exit 255
+fi
+if [ -z $2 ]
+then
+        current_name="basicpuppettemplate"
+else
+        current_name=$2
+fi        
+
+new_name=$1
+echo "Renaming module in file contents.."
+find ./ -type f | xargs sed -i s/"$current_name"/"$new_name"/g
+echo "Renaming module directory.."
+cd ..
+mv $current_name $new_name
+cd $new_name
