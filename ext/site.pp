1ac2469fb62e063357a46ab238064dc54b677d29
Alexandre Andrade <aleandrade@gmail.com>
2015-03-10 08:29:38 +1100

    Fixed allow_virtual issue

--- ext/site.pp
+++ ext/site.pp
@@ -8,6 +8,12 @@
 node default {
             hiera_include('classes','')
 }
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+          $allow_virtual_packages = hiera('allow_virtual_packages',false)
+            Package {
+                allow_virtual => $allow_virtual_packages,
+            }
+}
 #node master {
 #            hiera_include('classes','')
 #}
