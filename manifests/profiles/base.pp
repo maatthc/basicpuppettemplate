f5eed6608f4ca849ad8081a6527fd96dcbcb3fe9
Alexandre Andrade <aleandrade@gmail.com>
2015-03-09 21:12:35 +1100

    SSHD, users, groups and public keys added

--- manifests/profiles/base.pp
+++ manifests/profiles/base.pp
@@ -1,5 +1,17 @@
-# Class
-class rea::profiles::base {
+# Class Base - Define what is going to be applied to all hosts
+# Most of the settings are in the Hiera data files
+class rea::profiles::base (
+          $myusers,
+          $mygroups,
+          $ssh_authorized_keys
+        ){
+        # Define which packages should be installed and 
+        # which shouln't be present
         class { 'base::packages': }
+        # Define SSHD configuration for all servers
+        class { 'ssh': }
+        # Define user/groups to be created.
+        create_resources(group, $mygroups)
+        create_resources(user, $myusers)
+        create_resources(ssh_authorized_key, $ssh_authorized_keys)
 }
-
