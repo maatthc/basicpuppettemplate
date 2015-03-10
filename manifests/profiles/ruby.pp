535c1b401a61ab2b74689279209e1d014451e247
Alexandre Andrade <alexandre.andrade@ecetera.com.au>
2015-03-10 17:29:21 +1100

    Moving Gems list to Hiera

--- manifests/profiles/ruby.pp
+++ manifests/profiles/ruby.pp
@@ -1,6 +1,7 @@
 # Class that manages the Ruby Gems
-class rea::profiles::ruby {
-        # Ensure the required gems are present
+class rea::profiles::ruby (
+                $gems 
+        ){
         $gems = ['bundler', 'rack', 'sinatra', 'god', 'unicorn']
         package { $gems:
                   ensure        => 'installed',
