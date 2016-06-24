88c5ce81f8669a119d672819aa116086eb87e8d2
Alexandre Andrade <alexandre.andrade@ecetera.com.au>
2016-06-25 06:50:11 +1000

    Clean up comments

--- ext/site.pp
+++ ext/site.pp
@@ -1,19 +1,3 @@
-#node /^www\d+\.example\.com\.au$/ {
-#        class { '::rea': }
-#}
-
-#node 'default' {
-#        class { '::rea': }
-#}
 node default {
-            hiera_include('classes','')
+    hiera_include('classes','')
 }
-if versioncmp($::puppetversion,'3.6.1') >= 0 {
-          $allow_virtual_packages = hiera('allow_virtual_packages',false)
-            Package {
-                allow_virtual => $allow_virtual_packages,
-            }
-}
-#node master {
-#            hiera_include('classes','')
-#}
